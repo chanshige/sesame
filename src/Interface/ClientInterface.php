ecc7d88df497b3bef2708a5f456ae1a84499a274
Shigeki Tanaka <dev@shigeki.tokyo>
2024-01-09 16:33:02 +0900

    [fix] interface

--- src/Interface/ClientInterface.php
+++ src/Interface/ClientInterface.php
@@ -11,4 +11,14 @@ interface ClientInterface
 {
     /** @throws SesameException */
     public function __invoke(ActionInterface $action): ResponseInterface;
+
+    public function status(DeviceInterface $device): ResponseInterface;
+
+    public function history(DeviceInterface $device, int $page = 0, int $lg = 50): ResponseInterface;
+
+    public function lock(DeviceInterface $device, string $note = 'WebAPI'): ResponseInterface;
+
+    public function unLock(DeviceInterface $device, string $note = 'WebAPI'): ResponseInterface;
+
+    public function toggle(DeviceInterface $device, string $note = 'WebAPI'): ResponseInterface;
 }
